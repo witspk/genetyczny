8ca62249d59a3adad2b814f92aabec4ccfd37647
wit <witold.suryjak@student.pk.edu.pl>
2021-10-30 17:46:00 +0200

    Drop Wave

--- FitnessFunction.py
+++ FitnessFunction.py
@@ -1,6 +1,14 @@
+import math
+
+
 class FitnessFunction:
     def __init__(self):
         pass
 
+    # DROP-WAVE FUNCTION
     def value(self, x, y):
-        return 2 * pow(x, 2) + 2 * pow(y, 2) + 5
+        return -(1 + math.cos(12 * math.sqrt(pow(x, 2) + pow(y, 2)))) / (0.5 * (pow(x, 2) + pow(y, 2)) + 2)
+
+    # test function
+    # def value(self, x, y):
+    #     return 2 * pow(x, 2) + 2 * pow(y, 2) + 5
